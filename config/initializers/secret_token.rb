a39b3a5643fbbc1dca2f3b40a73b287153b268d8
jayteesf <jonathan@his-service.net>
2012-09-23 12:57:34 -0700

    initial: (started jruby, switched to MRI)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GhScore::Application.config.secret_token = '4114bbe2b07c5a9adfea30ac649dad6b279419a22959339ef4e1824bee0a5870c6228665e37581738770451e910b6b5fafa16b7da9328abef93a362e732aa36f'
