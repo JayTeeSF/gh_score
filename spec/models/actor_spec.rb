9805a3f88bc34c8b97925a3053ab51115cedb8bd
jayteesf <jonathan@his-service.net>
2012-09-23 20:02:01 -0700

    faster specs

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,28 +2,26 @@ require 'spec_helper'
 
 describe Actor do
   describe "import" do
-    let(:username) { "JayTeeSF" }
     let(:other_username) { "tenderlove" }
+    let(:mini_username) { "mini" }
 
     it "should be idempotent" do
       initial_event_count = Event.count
       lambda do
-        Actor.import username, :responder => FileResponder
+        Actor.import mini_username, :responder => FileResponder
       end.should change(Actor, :count).by(1)
-      expected_final_event_count = Event.count
-
-      (expected_final_event_count - initial_event_count).should be >= 1
+      Event.count.should eql( 1 )
 
       lambda do
-        Actor.import username, :responder => FileResponder
+        Actor.import mini_username, :responder => FileResponder
       end.should change(Actor, :count).by(0)
-      Event.count.should eql( expected_final_event_count )
+      Event.count.should eql( 1 )
     end
 
-    it "should import events for mulitple actors" do
+    it "should import events for multiple actors" do
       initial_event_count = Event.count
       lambda do
-        Actor.import username, :responder => FileResponder
+        Actor.import mini_username, :responder => FileResponder
         Actor.import other_username, :responder => FileResponder
       end.should change(Actor, :count).by(2)
       final_event_count = Event.count
@@ -34,11 +32,11 @@ describe Actor do
     it "should create an actor and her(/his) associated events" do
       initial_event_count = Event.count
       lambda do
-        Actor.import username, :responder => FileResponder
+        Actor.import mini_username, :responder => FileResponder
       end.should change(Actor, :count).by(1)
       final_event_count = Event.count
 
-      (final_event_count - initial_event_count).should be >= 1
+      (final_event_count - initial_event_count).should eql( 1 )
     end
   end
 end
