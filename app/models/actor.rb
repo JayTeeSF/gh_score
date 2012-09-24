5c3ff5b06414d428d863195ba5ebc1e26c71aa31
jayteesf <jonathan@his-service.net>
2012-09-23 20:52:56 -0700

    moved event-fetching logic from Actor to Event

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,30 +1,9 @@
-require 'digest/sha1'
 class Actor < ActiveRecord::Base
-  DEBUG = nil
   attr_accessible :name
 
   def self.import username, options = {}
-    actor = find_or_create_by_name username
-    event_records = Github::Client.fetch username, options[ :responder ]
-    event_records.each do |event_record|
-      event = Event.new
-      event.actor = actor
-
-      # mapping(s):
-      event.recorded_at = event_record["created_at"]
-      event.action = event_record["type"]
-
-      event.url = event_record["url"]
-
-      event.body = event_record.to_json
-      event.checksum = Digest::SHA1.hexdigest event.body
-      if event.valid?
-        puts "event is valid..." if DEBUG
-        event.save
-      else
-        puts event.errors.to_a.inspect if DEBUG
-      end
+    find_or_create_by_name( username ).tap do |actor|
+      Event.create_for actor, options
     end
-    actor
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,35 @@
+require 'digest/sha1'
 class Event < ActiveRecord::Base
+  DEBUG = nil
+
   belongs_to :actor
   attr_accessible :action, :checksum, :recorded_at, :url
   validates_uniqueness_of :checksum
+
+  def self.create_for actor, options = {}
+    event_records = options[ :event_records ] ||
+      Github::Client.fetch( actor.name, options[ :responder ] )
+
+    event_records.each do |event_record|
+      new.tap do |event|
+        event.actor = actor
+
+        # mapping(s):
+        event.recorded_at = event_record["created_at"]
+        event.action = event_record["type"]
+
+        event.url = event_record["url"]
+
+        event.body = event_record.to_json
+        event.checksum = Digest::SHA1.hexdigest event.body
+
+        if event.valid?
+          puts "event is valid..." if DEBUG
+          event.save
+        else
+          puts event.errors.to_a.inspect if DEBUG
+        end
+      end
+    end
+  end
 end
